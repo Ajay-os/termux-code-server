0439cb5a8498e677b61dda9df7d2346e04b4b61c
Ajay o s <akhubsmaster@gmail.com>
2022-04-19 08:40:38 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -15,8 +15,8 @@ apt install ./code-server_4.3.0_amd64.deb
 echo "starting export PASSWORD="1234567890" "
 export PASSWORD="1234567890"   
 echo "starting install updates"
-apt update
-apt install python
-apt install g++
+apt update -y
+apt install python -y
+apt install g++ -y
 echo "starting code server"
 code-server
